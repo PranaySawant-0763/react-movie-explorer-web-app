4a081e11d335b908fc4104c47844f1246820a52c
PranaySawant-0763 <pranaysawant763@gmail.com>
2026-02-14 20:55:55 +0530

    Initial commit - Movie Explorer Web App

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./css/App.css";
+import Favorites from "./pages/Favorites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import { MovieProvider } from "./contexts/MovieContext";
+
+function App() {
+  return (
+    <>
+      <MovieProvider>
+        <NavBar />
+        <div className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/favorites" element={<Favorites />} />
+          </Routes>
+        </div>
+      </MovieProvider>
+    </>
+  );
+}
+
+export default App;
